af19af0bc1f2726ce72d6298e9c8e4e49383f004
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-03-26 14:12:41 +0200

    :hammer: finished technical part

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,7 +1,25 @@
 import React from "react";
+import ReactEmoji from "react-emoji";
 
-const Message = ({ message, name }) => {
-  return <div></div>;
+const Message = ({ message: { user, text }, name }) => {
+  let iseSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+
+  if (user === trimmedName) {
+    iseSentByCurrentUser = true;
+  }
+  return iseSentByCurrentUser ? (
+    <div style={{ backgroundColor: "blue" }}>
+      <p>{trimmedName}</p>
+      <p>{ReactEmoji.emojify(text)}</p>
+    </div>
+  ) : (
+    <div style={{ backgroundColor: "#fff" }}>
+      <p>{user}</p>
+      <p>{ReactEmoji.emojify(text)}</p>
+    </div>
+  );
 };
 
 export default Message;
